3ef97d6dc25ae5fe79ad288b922a42866f88a53c
kartooon <56695892+kartooon@users.noreply.github.com>
2020-11-16 18:38:59 +0300

    Update z3.py

--- z3.py
+++ z3.py
@@ -7,7 +7,6 @@ from selenium.webdriver.support import expected_conditions as EC
 @pytest.fixture
 def driver(request):
     wd = webdriver.Chrome()
-    print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
 
@@ -16,4 +15,3 @@ def test_example(driver):
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("login").click()
-    #WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
