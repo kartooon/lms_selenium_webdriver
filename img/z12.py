7a98ea4e8523f878ea901a42f7029b00ccf137c5
kartooon <56695892+kartooon@users.noreply.github.com>
2020-12-03 22:09:26 +0300

    Update z12.py

--- img/z12.py
+++ img/z12.py
@@ -1,1 +1,78 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.alert import Alert
+from selenium import webdriver
+import os
+import datetime
+import pytest
+import random
+import string
 
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    return wd
+
+def random_string(key, len):
+    symbols = string.ascii_letters + string.digits
+    return "".join([random.choice(symbols) for i in range(len)])
+
+def set_general(driver, name):
+    driver.find_element_by_name("status").click()
+    driver.find_element_by_name("name[en]").send_keys(name)
+    driver.find_element_by_name("code").send_keys("1111")
+    driver.find_element_by_css_selector("[value='1-1']").click()
+    driver.find_element_by_name("quantity").clear()
+    driver.find_element_by_name("quantity").send_keys("1")
+    path = os.path.abspath(r"img/crya.jpeg")
+    driver.find_element_by_name("new_images[]").send_keys(path)
+    date_from = datetime.date.today()
+    driver.find_element_by_name("date_valid_from").send_keys(date_from.strftime('%Y-%m-%d'))
+    date_to = datetime.date.today() + datetime.timedelta(days=15)
+    driver.find_element_by_name("date_valid_to").send_keys(date_to.strftime('%Y-%m-%d'))
+    driver.find_element_by_css_selector("[href='#tab-information']").click()
+
+
+def set_information(driver):
+    Select(driver.find_element_by_name("manufacturer_id")).select_by_visible_text("ACME Corp.")
+    driver.find_element_by_name("keywords").send_keys("keywords")
+    driver.find_element_by_name("short_description[en]").send_keys("short_description")
+    driver.find_element_by_name("description[en]").send_keys("description<br>description<br>description")
+    driver.find_element_by_name("head_title[en]").send_keys("head_title")
+    driver.find_element_by_name("meta_description[en]").send_keys("meta_description")
+    driver.find_element_by_css_selector("[href='#tab-prices']").click()
+
+
+def set_prices(driver):
+    driver.find_element_by_name("purchase_price").clear()
+    driver.find_element_by_name("purchase_price").send_keys("1")
+    Select(driver.find_element_by_name("purchase_price_currency_code")).select_by_value("USD")
+    driver.find_element_by_name("gross_prices[USD]").clear()
+    driver.find_element_by_name("gross_prices[USD]").send_keys("13")
+    driver.find_element_by_name("gross_prices[EUR]").clear()
+    driver.find_element_by_name("gross_prices[EUR]").send_keys("13")
+    driver.find_element_by_xpath("//button[@name='save']").click()
+
+
+def check_product(driver, name):
+    driver.find_element_by_link_text(name).click()
+    driver.find_element_by_name("delete").click()
+    Alert(driver).accept()
+
+
+def test_example(driver):
+    driver.get("http://localhost/litecart/admin/")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
+    wait = WebDriverWait(driver, 10)
+
+    driver.find_element_by_css_selector("[href$='/admin/?app=catalog&doc=catalog']").click()
+    driver.find_element_by_css_selector("[href$='/admin/?category_id=0&app=catalog&doc=edit_product']").click()
+
+    name = random_string("", 10)
+
+    set_general(driver, name)
+    set_information(driver)
+    set_prices(driver)
+    check_product(driver, name)
